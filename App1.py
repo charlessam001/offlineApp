243ec8023721698906828763525a1969acb39f8c
charlessam001 <charlessam728@gmail.com>
2024-07-01 16:25:58 +0200

    Update App1.py

--- App1.py
+++ App1.py
@@ -1,1 +1,24 @@
+"""from kivymd.app import MDApp
+from kivymd.uix.screen import Screen
+from kivymd.uix.button import MDFlatButton, MDRectangleFlatButton
+from kivymd.uix.label import MDLabel, MDIcon """
+from secondfile import *
+
+SecondApp().run()
+
+"""class jjj(MDApp):
+    def build(self):
+        screen = Screen()
+        btn = MDRectangleFlatButton(text='Bonjour', pos_hint={'center_x': 0.4, 'center_y': 0.4})
+        btn_flat = MDFlatButton(text='okay', pos_hint={'center_x': 0.4, 'center_y': 0.3})
+        label = MDLabel(text='Good', halign='center')
+        icon_label = MDIcon(icon='language-python', pos_hint={'center_x': 0.4, 'center_y': 0.6})
+        screen.add_widget(btn_flat)
+        screen.add_widget(label)
+        screen.add_widget(icon_label)
+        screen.add_widget(btn)
+        return screen
+
+jjj().run()
+"""
 
